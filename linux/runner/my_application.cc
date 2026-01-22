466c370b1a2f4b302289aec9727d3316cdc5cc4f
krasyid822 <krasyid822@gmail.com>
2026-01-22 09:28:31 +0700

    Refactor code structure for improved readability and maintainability

--- linux/runner/my_application.cc
+++ linux/runner/my_application.cc
@@ -45,11 +45,11 @@ static void my_application_activate(GApplication* application) {
   if (use_header_bar) {
     GtkHeaderBar* header_bar = GTK_HEADER_BAR(gtk_header_bar_new());
     gtk_widget_show(GTK_WIDGET(header_bar));
-    gtk_header_bar_set_title(header_bar, "clark_deliv_eats");
+    gtk_header_bar_set_title(header_bar, "foodie.ID");
     gtk_header_bar_set_show_close_button(header_bar, TRUE);
     gtk_window_set_titlebar(window, GTK_WIDGET(header_bar));
   } else {
-    gtk_window_set_title(window, "clark_deliv_eats");
+    gtk_window_set_title(window, "foodie.ID");
   }
 
   gtk_window_set_default_size(window, 1280, 720);
